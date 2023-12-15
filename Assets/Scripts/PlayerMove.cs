37ec697327674f4c4e0bbadc35f0e9154bc2e69c
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-12-14 21:37:20 -0300

    qsy walljump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -14,6 +14,8 @@ public class PlayerMove : MonoBehaviour
     private bool isGrounded = false;
     private bool isFacingRight = true;
     private bool isTouchingWall = false;
+    private bool isWallJumping = false;
+    private Vector2 wallJumpDirection;
     private Animator animator;
 
     //test of particular system (?
@@ -31,22 +33,25 @@ public class PlayerMove : MonoBehaviour
 
         //set animations
         animator.SetFloat("Horizontal", Mathf.Abs(moveDirection));
-        
+
         animator.SetBool("OnGround", isGrounded);
-        
+
         animator.SetBool("TouchingWall", isTouchingWall);
 
         // Move the player horizontally
-        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (!isWallJumping)
+        {
+            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
 
-        // Flip the player's direction if needed
-        if (moveDirection > 0 && !isFacingRight)
-        {
-            Flip();
-        }
-        else if (moveDirection < 0 && isFacingRight)
-        {
-            Flip();
+            // Flip the player's direction if needed
+            if (moveDirection > 0 && !isFacingRight)
+            {
+                Flip();
+            }
+            else if (moveDirection < 0 && isFacingRight)
+            {
+                Flip();
+            }
         }
     }
 
@@ -55,34 +60,78 @@ public class PlayerMove : MonoBehaviour
         // Check for jump input when grounded or touching a wall
         if ((isGrounded || isTouchingWall) && Input.GetKeyDown(KeyCode.Space))
         {
-            
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            particles.Play();
+            if (isTouchingWall)
+            {
+                WallJump();
+            }
+            else
+            {
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                particles.Play();
+            }
         }
+    }
+
+    void WallJump()
+    {
+        
+        Vector2 jumpDirection;
+
+        if (isTouchingWall)
+        {
+            
+            float angleInRadians = Mathf.PI / 4; 
+            jumpDirection = new Vector2(Mathf.Cos(angleInRadians) * -wallJumpDirection.x, Mathf.Sin(angleInRadians)).normalized;
+
+            
+            if (jumpDirection.x > 0 && !isFacingRight)
+            {
+                Flip();
+            }
+            else if (jumpDirection.x < 0 && isFacingRight)
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            
+            float angleInRadians = Mathf.PI / 4; 
+            jumpDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)).normalized;
+        }
+
+        
+        rb.velocity = jumpDirection * Mathf.Sqrt(2 * jumpForce * jumpForce); // Mantener la velocidad
+        particles.Play();
+        isWallJumping = true;
+        StartCoroutine(DisableWallJump());
+    }
+
+    IEnumerator DisableWallJump()
+    {
+        yield return new WaitForSeconds(0.1f);  // Adjust the time based on your needs
+        isWallJumping = false;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        
         if (other.gameObject.CompareTag(groundTag))
         {
             isGrounded = true;
         }
-        
         else if (other.gameObject.CompareTag("Wall"))
         {
             isTouchingWall = true;
+            wallJumpDirection = other.contacts[0].normal * -1;  // Store the wall direction
         }
     }
 
     void OnCollisionExit2D(Collision2D other)
     {
-        
         if (other.gameObject.CompareTag(groundTag))
         {
             isGrounded = false;
         }
-        
         else if (other.gameObject.CompareTag("Wall"))
         {
             isTouchingWall = false;
