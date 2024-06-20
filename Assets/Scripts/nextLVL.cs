3af9ae5cc87ec7cc4769b1932381b41465e9d7a9
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2024-06-20 10:59:30 -0300

    casi

--- Assets/Scripts/nextLVL.cs
+++ Assets/Scripts/nextLVL.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class nextLVL : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DeleteSaveData();
         if (collision.gameObject.CompareTag("Player"))
         {
             // next escene in the build
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    private void DeleteSaveData()
+    {
+        string path = Application.persistentDataPath + "/player.carpincho";
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            Debug.Log("Save data deleted.");
+        }
+        else
+        {
+            Debug.Log("No save data to delete.");
         }
     }
 }
